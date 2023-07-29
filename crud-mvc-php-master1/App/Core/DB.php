337387be9e95c4097b325397b5a17b402d45ce28
Ismail910 <ismailmagdy566@gmail.com>
2023-07-29 04:22:16 +0300

    marge

--- crud-mvc-php-master1/App/Core/DB.php
+++ crud-mvc-php-master1/App/Core/DB.php
@@ -1,35 +1,199 @@
-<?php 
+<?php
 
 
-require_once ('DB/MysqliDb.php');
 
 class DB 
 {
-    protected $db;
 
-    public function connect()
+    private $pdo="mysql:dbname=".DB_NAME.";host=".DB_HOST.";port=".DB_PORT."";
+    public $conn;
+
+    function __construct()
     {
-        $this->db = new MysqliDb (DB_HOST, DB_USER, DB_PASS, DB_NAME);
-        if(!$this->db)
-            die("Connection Error : ");
-        return $this->db;
+        try
+        {
+        $this->conn=new PDO($this->pdo,DB_USER,DB_PASS);
+        }catch(PDOException $e)
+        {
+            echo $e->getMessage();
+        }
+
     }
 
-    public function searchUsers($searchQuery)
+    // public function connect(){
+    //     try {
+    //        $this->conn = new PDO($this->pdo, DB_USER, DB_PASS);
+    //         return $this->conn ;
+    
+    //     } catch (Exception $e){
+    //         echo $e->getMessage();
+    //     }
+    // }
+    /*public function selectUserByRole($q){
+        $query =$q;
+        $select_stmt = $this->conn->prepare($query);
+        $res=$select_stmt->execute();
+        $data = $select_stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+    }*/
+    function select($table)
     {
-        $searchQuery = '%' . $searchQuery . '%';
+        try
+        {
+        $query="select *from {$table}";
+        $stmt=$this->conn->prepare($query);
+        $stmt->execute();
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }catch(Exception $e)
+        {
+            return false;
+        }
 
-        $this->db->where('id', $searchQuery, 'LIKE');
-        $this->db->orWhere('name', $searchQuery, 'LIKE');
-        $this->db->orWhere('position', $searchQuery, 'LIKE');
-        $this->db->orWhere('department', $searchQuery, 'LIKE');
-        $this->db->orWhere('phone_number', $searchQuery, 'LIKE');
-        $this->db->orWhere('start_date', $searchQuery, 'LIKE');
-        $this->db->orWhere('end_date', $searchQuery, 'LIKE');
-        $this->db->orWhere('status', $searchQuery, 'LIKE');
-
-        return $this->db->get('users');
     }
 
+
+    function selectById($table,$id)
+    {
+        try
+        {
+        $query="select *from {$table} where id=:id";
+        $stmt=$this->conn->prepare($query);
+        $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+        $stmt->execute();
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+        }catch(Exception $e)
+        {
+            return false;
+        }
+
+    }
+
+
+    function insert($table,$arr)
+    {
+        try 
+        {
+        $query="insert into `{$table}` (";
+
+        foreach($arr as $k=>$v)
+        {
+            $query.="`$k`,";
+
+        }
+        $query=trim($query,',');
+
+        $query.=") values (";
+
+        foreach($arr as $k=>$v)
+        {
+            $query.=":$k ,";
+        }
+        $query=trim($query,',');
+        $query.=")";
+
+        $stmt=$this->conn->prepare($query);
+    
+
+        foreach($arr as $k=>&$v)
+        {
+    
+            $stmt->bindParam(":$k",$v);
+        }
+
+    
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+
+      }catch(PDOException $e)
+      {
+       return false;
+      }
+
+        
+
+
+
+    }
+
+
+    function update($table,$id,$arr)
+    {
+
+        try 
+        {
+        $query="update  `{$table}` set ";
+
+      
+        foreach($arr as $k=>$v)
+        {
+            if($k!='id')
+            $query.=" `$k` = :$k ,";
+        }
+        $query=rtrim($query,',');
+        $query.="where `id`= :id";
+
+        $stmt=$this->conn->prepare($query);
+
+
+        foreach($arr as $k=>&$v)
+        {
+            if($k!='id')
+            $stmt->bindParam(":$k",$v);
+        }
+    
+        $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+   
+
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+
+      }catch(PDOException $e)
+      {
+        return false;
+      }
+
+    }
+
+
+    function delete($table,$id)
+    {
+        try 
+        {
+        $user=$this->selectById($table,$id);
+        $query="delete from {$table} where id=:id";
+        $stmt=$this->conn->prepare($query);
+        $stmt->bindParam(":id",$id,PDO::PARAM_INT);
+        if($stmt->execute()){
+            return true;
+        }else{
+            return false;
+        }
+        }catch(PDOException $e)
+        {
+            return false;
+        }
+
+    }
+
+
+    function rawQuery($query)
+    {
+        $select_stmt = $this->conn->prepare($query);
+        if($select_stmt->execute()){
+        $data = $select_stmt->fetchAll(PDO::FETCH_ASSOC);
+        return $data;
+        }
+        return false;
+    }
 
 }
+
+
+
+?>
